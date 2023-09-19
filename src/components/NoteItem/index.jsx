4ebf9ac837d0c00bf2700e1522b092c0da6e1338
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-09-19 18:54:35 -0300

    Atualização

--- src/components/NoteItem/index.jsx
+++ src/components/NoteItem/index.jsx
@@ -1,7 +1,7 @@
 import {FiPlus, FiX} from 'react-icons/fi';
 import {Container} from './styles';
 
-export function NoteItem({isNew, value, onClick, ...rest}){
+export function NoteItem({isNew = false, value, onClick, ...rest}){
    return(
       <Container isNew={isNew}>
          <input type="text" value={value} readOnly={!isNew} {...rest} />
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
-import {Button} from "../../components/Button";
+import { Button } from "../../components/Button";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import { Container, Form } from "./styles";
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState(""); 
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState(""); 
+
+  function handleAddLink() {
+    setLinks((prevState) => [...prevState, newLink]); //estado anterior acessar pelo prevState
+    setNewLink(""); // LIMPAR O CAMPO
+  }
+
+  function handleRemoveLink(deleted){ // retorna todos links menos o link que eu deletar
+    setLinks(prevState => prevState.filter(link => link !== deleted));// armazena no setLinks
+  }
+
+  function handleAddTag(){// prevstate tudoo que estava antes
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("")
+  }
+
+  function handleDeleteTag(deleted){
+    setTags(prevState => prevState.filter(tag => tag !== deleted))
+  }
+
   return (
     <Container>
       <Header />
@@ -24,17 +49,44 @@ export function New() {
           <Textarea placeholder="Observação" />
 
           <Section title="Links úteis">
-            <NoteItem value="https://www.rocketseat.com.br/" />
-            <NoteItem placeholder="Novo link" isNew />
+            {links.map((link,index) => (// buscar informações que está no estado link
+              <NoteItem
+                key={String(index)}
+                value={link} // pegar valor
+                onClick={() => handleRemoveLink(link)}
+              />
+            ))}
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
-          <Section>
+          <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value="react" />
-              <NoteItem placeholder="Novo Tag" isNew />
+            {
+              tags.map((tag, index)=>(
+                <NoteItem
+                key={String(index)} // recebe a posição
+                 value={tag} 
+                  onClick={()=> handleDeleteTag(tag)} // passa tag para deletar
+                 />
+              ))
+            }
+
+              <NoteItem 
+              isNew 
+              placeholder="Novo Tag"
+              onChange={e => setNewTag(e.target.value)}
+              value={newTag}
+              onClick={handleAddTag}
+               />
             </div>
           </Section>
-          <Button title="Salvar"/>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
