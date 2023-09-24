303834eb955baaa2777c473037fa3b8a090128ec
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-09-24 10:51:38 -0300

    Atualização

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from './styles//global';
+import GlobalStyles from './styles/global';
 
 import { AuthProvider } from "./hooks/auth";
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['componentes estilizados'], // Substitua 'componentes estilizados' pelo nome correto do módulo
+    },
+  },
 })
