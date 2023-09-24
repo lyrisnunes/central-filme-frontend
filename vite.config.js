4f323684988816be7c436c2fdc30593e830bc36c
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-09-24 10:55:43 -0300

    Alteração

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['componentes estilizados'], // Substitua 'componentes estilizados' pelo nome correto do módulo
+      external: ['styled-components'],
     },
   },
 })
