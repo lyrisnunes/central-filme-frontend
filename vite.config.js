48ebbcd6c70f8cf933f5472fbf1de9968ec9b877
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-09-24 11:37:45 -0300

    Novo

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['styled-components', 'axios', 'react-icons/fi', 'react-icons/ri'],
-         
-    },
-  },
 })
