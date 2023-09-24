b29f3f46ccfc80d899331652e092fc178e497205
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-09-24 15:33:40 -0300

    Atualização styled

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
-
 import { AuthProvider } from "./hooks/auth";
-
+import theme  from "./styles/theme";
 import { Routes } from "./routes";
 
-import theme  from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
